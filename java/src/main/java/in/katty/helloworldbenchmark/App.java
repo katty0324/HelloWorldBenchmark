5595b2e721db15d96bc1de2465b8c2e7db087dd4
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2014-07-14 00:22:20 +0900

    Add Java hello world app

---
+++ java/src/main/java/in/katty/helloworldbenchmark/App.java
@@ -0,0 +1,17 @@
+package in.katty.helloworldbenchmark;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class App extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.getWriter().println("<!doctype html><html><body>Hello Java!</body></html>");
+	}
+
+}
